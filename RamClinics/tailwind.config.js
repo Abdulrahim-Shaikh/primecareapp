94ac145011da5c53cf766ce1123762a3ad900be7
Naveedur Rahman <naveed@technassolution.com>
2024-09-25 15:51:36 +0300

    Initial Commit

---
+++ RamClinics/tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+    colors: {
+      white: "rgba(255, 255, 255, 1)",
+      primaryColor: "#009281",
+      secondaryBg: "rgba(230, 244, 242, 1)",
+      neutralColor: "rgba(14, 13, 57, 1)",
+      bodyText: "#454567",
+      borderColor: "#c3c3ce",
+    },
+  },
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
