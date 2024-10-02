a79622f6b9972a572577a937f9dea6f0ca7d35d6
Naveedur Rahman <naveed@technassolution.com>
2024-10-02 20:55:36 +0300

    API update

--- RamClinics/domain/services/LoginService.ts
+++ RamClinics/domain/services/LoginService.ts
@@ -1,13 +1,29 @@
 import { RestService } from "./core/RestService";
 
-export class LoginService extends RestService<any>{
+export class LoginService extends RestService<any> {
 
     generateOtp(mobileNo: any) {
+        mobileNo = this.formateMobileNo(mobileNo);
         return this.get("generateOtp/" + mobileNo);
     }
 
     byMobileNo(mobileNo: any) {
+        mobileNo = this.formateMobileNo(mobileNo);
         return this.get("byMobileNo/" + mobileNo);
+    }
+
+
+    formateMobileNo(mobileNo: string) {
+        if (mobileNo.startsWith("0")) {
+            mobileNo = mobileNo.substring(1);
+        }
+        if (mobileNo.startsWith("966")) {
+            mobileNo = mobileNo.substring(3);
+        }
+        if (mobileNo.startsWith("+966")) {
+            mobileNo = mobileNo.substring(4);
+        }
+        return mobileNo;
     }
 }
 const loginService = new LoginService('signupform');
