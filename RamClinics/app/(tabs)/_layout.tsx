71621478166bb6668e31b9adf42bec6eb61dc13c
Naveedur Rahman <naveed@technassolution.com>
2024-10-03 21:51:04 +0300

    Home page icons update

--- RamClinics/app/(tabs)/_layout.tsx
+++ RamClinics/app/(tabs)/_layout.tsx
@@ -1,28 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { Tabs } from "expo-router";
-import Entypo from "@expo/vector-icons/Entypo";
 import {
-  AntDesign, FontAwesome, MaterialCommunityIcons
+  AntDesign, FontAwesome
 } from "@expo/vector-icons";
-import { useUserSate } from "../../domain/state/UserState";
 
-const TabIcon = ({
-  focused,
-  iconName,
-}: {
+type TabIconProbs = {
   focused: boolean;
   iconName: string;
-}) => {
+}
+
+const TabIcon = ({ focused, iconName, }: TabIconProbs) => {
   return (
     <View className="flex items-center justify-center gap-2">
-      <Text
-        className={`p-2 justify-center justify-items-center rounded-lg ${focused ? "bg-lime-500" : "bg-white"}`}
-      >
+      <Text className={`p-2 justify-center justify-items-center rounded-lg ${focused ? "bg-lime-500" : "bg-white"}`} >
         {iconName === "home" && (
           <>
             <View className="flex flex-col justify-center justify-items-center justif-self-center">
-              <Entypo name="home" size={24} color={focused ? "white" : "rgb(120 53 15)"} />
+              {/* <Entypo name="home" size={28} color={focused ? "white" : "rgb(120 53 15)"} /> */}
+              <FontAwesome name="home" size={28} color={focused ? "white" : "rgb(120 53 15)"} />
               {/* <Text className={`text-xs text-center ${focused ? " text-white" : "text-amber-900"}`}>Home</Text> */}
             </View>
           </>
@@ -37,24 +33,18 @@ const TabIcon = ({
         {iconName === "calendar-check-outline" && (
           <>
             <View className="flex flex-col justify-center justify-items-center justif-self-center">
-              <MaterialCommunityIcons
-                name="calendar-check-outline"
-                size={24}
-                color={focused ? "white" : "rgb(120 53 15)"}
-              />
+              {/* <MaterialCommunityIcons name="calendar-check-outline" size={28} color={focused ? "white" : "rgb(120 53 15)"} /> */}
+              <FontAwesome name="calendar-check-o" size={28} color={focused ? "white" : "rgb(120 53 15)"} />
               {/* <Text className={`text-xs text-center ${focused ? " text-white" : "text-amber-900"}`}>My{"\n"}Appointments</Text> */}
             </View>
           </>
         )}
-        {iconName === "book-clock-outline" && (
+        {iconName === "book-app" && (
           <>
-            <View className="flex flex-col justify-center justify-items-center justif-self-center">
-              <MaterialCommunityIcons
-                name="book-clock-outline"
-                size={44}
-                color={focused ? "white" : "rgb(120 53 15)"}
-              />
-              {/* <Text className={`text-xs text-center ${focused ? " text-white" : "text-amber-900"}`}>Book{"\n"}Appointment</Text> */}
+            <View className="flex flex-col justify-center justify-items-center justif-self-center mt-4 " >
+              {/* <MaterialCommunityIcons name="book-clock-outline" size={44} color={focused ? "white" : "rgb(120 53 15)"} /> */}
+              <FontAwesome className="text-center" name="calendar-plus-o" size={28} color={focused ? "white" : "rgb(120 53 15)"} />
+              <Text className={`text-xs text-center pt-1 ${focused ? " text-white" : "text-amber-900"}`}>Book{"\n"}Appointment</Text>
             </View>
 
           </>
@@ -62,23 +52,15 @@ const TabIcon = ({
         {iconName === "gift" && (
           <>
             <View className="flex flex-col justify-center justify-items-center justif-self-center">
-              <FontAwesome
-                name="gift"
-                size={24}
-                color={focused ? "white" : "rgb(120 53 15)"}
-              />
-
+              <FontAwesome name="gift" size={28} color={focused ? "white" : "rgb(120 53 15)"} />
               {/* <Text className={`text-xs text-center ${focused ? " text-white" : "text-amber-900"}`}>Promotions</Text> */}
             </View>
           </>
         )}
         {iconName === "user" && (
           <>
-            <View className="flex flex-col justify-center justify-items-center justif-self-center"><AntDesign
-              name="user"
-              size={24}
-              color={focused ? "white" : "rgb(120 53 15)"}
-            />
+            <View className="flex flex-col justify-center justify-items-center justif-self-center">
+              <AntDesign name="user" size={28} color={focused ? "white" : "rgb(120 53 15)"} />              
               {/* <Text className={`text-xs text-center ${focused ? " text-white" : "text-amber-900"}`}>Profile</Text> */}
             </View>
           </>
@@ -111,7 +93,7 @@ const TabLayout = () => {
 
           tabBarStyle: {
             backgroundColor: "rgb(120 53 15)",
-            height: 90,
+            height: 100,
             borderTopEndRadius: 15,
             borderTopLeftRadius: 15,
             borderTopWidth: 0,
@@ -123,6 +105,7 @@ const TabLayout = () => {
           name="index"
           options={{
             headerShown: false,
+            title: 'Home',
             tabBarIcon: ({ color, focused }) => (
               <TabIcon iconName="home" focused={focused} />
             ),
@@ -145,6 +128,7 @@ const TabLayout = () => {
           name="MyAppoinment"
           options={{
             headerShown: false,
+            title: 'MyAppoinment',
             tabBarIcon: ({ color, focused }) => (
               <TabIcon iconName="calendar-check-outline" focused={focused} />
             ),
@@ -157,8 +141,9 @@ const TabLayout = () => {
           name="BookAppointment"
           options={{
             headerShown: false,
+            title: "",
             tabBarIcon: ({ color, focused }) => (
-              <TabIcon iconName="book-clock-outline" focused={focused} />
+              <TabIcon iconName="book-app" focused={focused} />
             ),
           }}
         />
@@ -168,8 +153,9 @@ const TabLayout = () => {
           name="Promotions"
           options={{
             headerShown: false,
+            title: "Promotions",
             tabBarIcon: ({ color, focused }) => (
-              <TabIcon iconName="gift" focused={focused} />
+              <TabIcon iconName="gift" focused={focused} />              
             ),
           }}
         />
@@ -178,6 +164,7 @@ const TabLayout = () => {
           name="ProfileTab"
           options={{
             headerShown: false,
+            title: "Profile",
             tabBarIcon: ({ color, focused }) => (
               <TabIcon iconName="user" focused={focused} />
             ),
